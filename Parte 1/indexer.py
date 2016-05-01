1a41804431b413f5264ad2be7c04204b335e1430
You Name <you@exemplo.com>
2016-05-01 16:47:23 -0300

    Continuando Index

--- Parte 1/indexer.py
+++ Parte 1/indexer.py
@@ -1,4 +1,6 @@
+import math
 import numpy as np
+
 
 
 
@@ -11,6 +13,9 @@ class IndexerInvertedList(object):
 		self.terms_documents = []
 		self.terms_number = None
 		self.documents_number = None
+		self.tf = []
+		self.idf = []
+		self.tf_idf = []
 
 	#Reading the config file for get the paths of files
 	def get_paths_files(self, files_list):
@@ -42,8 +47,8 @@ class IndexerInvertedList(object):
 
 		files.close()
 
-	#Recorving the quantity of terms and documents	
-	def get_border(self, line):
+	#Recovering the quantity of terms and documents, and create the terms documents matrix
+	def create_matrices(self, line):
 
 
 		splited = line.split(";")
@@ -53,20 +58,39 @@ class IndexerInvertedList(object):
 		self.terms_number = int(quantities[0])
 		self.documents_number = int(quantities[1])
 
+		self.terms_documents = np.zeros((self.terms_number, self.documents_number)) #term-document matrix
 
-	#Reading csv file that content the inverted list	
+		self.tf =  np.zeros((self.terms_number, self.documents_number)) #tf matrix
+
+		self.idf = np.zeros((self.terms_number, 1)) #idf list 
+
+	#Reading csv file that content the inverted list and create matrices
 	def read_csv_file(self, csv_file_path):
 
 		csv_file = open(csv_file_path, 'r')
 
-		self.get_border(csv_file.readline())
+		self.create_matrices(csv_file.readline())
 
 		for line in csv_file.readlines():
 
 			splited = line.split(";")
 
-			self.terms.append
+			self.terms.append(splited[0])
+
+			term_index = self.terms.index(splited[0])
+
+			documents = splited[1][1:-2].split(",")
+
+			self.idf[term_index] = math.log(self.documents_number/len(documents))
+
 			
+			for dci in documents:
+				self.terms_documents[term_index, (int(dci)-1)] += 1 #Filling the matrix with frequency of terms in documents
+				self.tf[term_index, (int(dci)-1)] = 1 + math.log(self.terms_documents[term_index, (int(dci)-1)]) #Filling the tf matrix
+
+
+		print(self.tf[9][499])
+		print(self.idf[9])		
 
 
 		
